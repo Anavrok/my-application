bae4f571a9328a5d022656e657c39c45224ef44a
matada2000 <matada200060@gmail.com>
2022-01-10 23:03:22 +0100

    2.7

--- app/src/main/java/com/taimoor/projekt_labirynt/Mapa.java
+++ app/src/main/java/com/taimoor/projekt_labirynt/Mapa.java
@@ -1,38 +1,121 @@
 package com.taimoor.projekt_labirynt;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentActivity;
-
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.net.Uri;
 import android.os.Bundle;
-
+import android.Manifest;
+import android.provider.Settings;
+import android.widget.SearchView;
+import android.widget.Toast;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+import java.io.IOException;
+import java.util.List;
 
-public class Mapa extends FragmentActivity implements OnMapReadyCallback {
+public class Mapa extends AppCompatActivity implements OnMapReadyCallback {
 
-    GoogleMap map;
+    SearchView searchview;
+    GoogleMap mGoogleMap;
+    boolean isPermissionGranted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapa);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        checkMyPermission();
+
+        if(isPermissionGranted)
+        {
+            SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
+            supportMapFragment.getMapAsync(this);
+        }
+
+        searchview = findViewById(R.id.MapSearchView);
+        searchview.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                String location = searchview.getQuery().toString();
+                List<Address> addressList = null;
+
+                if (location != null || location.equals(""))
+                {
+                    Geocoder geocoder = new Geocoder(Mapa.this);
+                    try {
+                        addressList = geocoder.getFromLocationName(location, 1);
+                    } catch (IOException e)
+                    {
+                        e.printStackTrace();
+                    }
+
+                    Address address = addressList.get(0);
+                    LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+
+                    mGoogleMap.addMarker(new MarkerOptions().position(latLng).title(location));
+
+                    mGoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                }
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
     }
+
+    private void checkMyPermission()
+    {
+        Dexter.withContext(this).withPermission(Manifest.permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener()
+        {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse)
+            {
+                Toast.makeText(Mapa.this, "Masz dostęp", Toast.LENGTH_SHORT).show();
+                isPermissionGranted = true;
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse)
+            {
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                Uri uri = Uri.fromParts("package", getPackageName(),"");
+                intent.setData(uri);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken)
+            {
+                permissionToken.continuePermissionRequest();
+            }
+        }).check();
+    }
+
+    @SuppressLint("MissingPermission")
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        map = googleMap;
-
-        LatLng NowySacz = new LatLng(49.6091403188255, 20.704296224569315);
-        map.addMarker(new MarkerOptions().position(NowySacz).title("Nowy Sącz"));
-        map.moveCamera(CameraUpdateFactory.newLatLng(NowySacz));
-
+    public void onMapReady(GoogleMap googleMap)
+    {
+        mGoogleMap = googleMap;
+        mGoogleMap.setMyLocationEnabled(true);
     }
+
 }
