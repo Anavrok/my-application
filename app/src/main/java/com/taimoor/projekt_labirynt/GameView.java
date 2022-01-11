b3031591f2b7a4868d54f72fac682759d852c59f
Maciej Cetera <maciek1128@gmail.com>
2022-01-11 10:36:20 +0100

    2.8

--- app/src/main/java/com/taimoor/projekt_labirynt/GameView.java
+++ app/src/main/java/com/taimoor/projekt_labirynt/GameView.java
@@ -33,15 +33,28 @@ public class GameView extends View {
     private static int COLS = 6, ROWS = 12;
     private static final float WALL_THICKNESS = 4;
     private float cellSize, hMargin, vMargin;
-    private Paint wallPaint, playerPaint, exitPaint;
+    private Paint wallPaint, playerPaint, exitPaint, text, time, point, koniec;
     private Random random;
-    private static float licznik_col = 0;
-    private  static float punkty = 0;
+    private static float licznik = 0;
+    private  static double punkty = 0;
+    private boolean timeExpired;
+    private long startTime;
+    private int r= 255, g= 255, b = 255;
+    private int size = 40;
 
 
     public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
 
+        startTime = System.currentTimeMillis();
+        text = new Paint();
+        text.setColor(Color.RED);
+
+        time = new Paint();
+        time.setColor(Color.RED);
+
+        point = new Paint();
+        point.setColor(Color.RED);
 
         wallPaint = new Paint();
         wallPaint.setColor(Color.BLACK);
@@ -49,6 +62,10 @@ public class GameView extends View {
 
         playerPaint = new Paint();
         playerPaint.setColor(Color.RED);
+
+        koniec = new Paint();
+        koniec.setColor(Color.MAGENTA);
+        koniec.setStrokeWidth(50);
 
         exitPaint = new Paint();
         exitPaint.setColor(Color.BLUE);
@@ -110,102 +127,127 @@ public class GameView extends View {
         }
     }
 
+
     private void createMaze(){
         Stack<Cell> stack = new Stack<>();
         Cell current, next;
 
         cells = new Cell[COLS][ROWS];
 
-        for(int x=0; x<COLS; x++){
-            for(int y=0; y<ROWS; y++){
-                cells[x][y] = new Cell(x,y);
+        for (int x = 0; x < COLS; x++) {
+            for (int y = 0; y < ROWS; y++) {
+                cells[x][y] = new Cell(x, y);
             }
         }
 
         player = cells[0][0];
-        exit = cells[COLS-1][ROWS-1];
+        exit = cells[COLS - 1][ROWS - 1];
 
         current = cells[0][0];
         current.visited = true;
 
         do {
-        next = getNeighbour(current);
-        if(next != null){
-            removeWall(current, next);
-            stack.push(current);
-            current = next;
-            current.visited = true;
-        }
-        else{
-            current = stack.pop();
-        }}while (!stack.empty());
+            next = getNeighbour(current);
+            if (next != null) {
+                removeWall(current, next);
+                stack.push(current);
+                current = next;
+                current.visited = true;
+            } else {
+                current = stack.pop();
+            }
+        } while (!stack.empty());
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawColor(Color.LTGRAY);
+        canvas.drawColor(Color.rgb(r,g,b));
 
-        int width = getWidth();
-        int height = getHeight();
+        long timeNow = System.currentTimeMillis();
+        long timeToGo = 100 - (timeNow - startTime) / 1000;
 
-        if(width/height < COLS/ROWS)
-            cellSize = width/(COLS+1);
-        else
-            cellSize = height/(ROWS+1);
+        if (timeToGo > 0) {
+            text.setTextSize(size);
+            time.setTextSize(size);
+            point.setTextSize(size);
 
-        hMargin = (width-COLS*cellSize)/2;
-        vMargin = (height-ROWS*cellSize)/2;
 
-        canvas.translate(hMargin, vMargin);
+            canvas.drawText("Czas: ", 10, 35, time);
+            canvas.drawText(Long.toString(timeToGo), 120, 35, text);
+            canvas.drawText("Punkty: " + String.format("%.2f", punkty), 220, 35, point);
 
-        for(int x=0; x<COLS; x++){
-            for(int y=0; y<ROWS; y++){
-                if (cells[x][y].topWall)
-                    canvas.drawLine(
-                        x*cellSize,
-                            y*cellSize,
-                            (x+1)*cellSize,
-                            y*cellSize,
-                            wallPaint);
-                if (cells[x][y].leftWall)
-                    canvas.drawLine(
-                            x*cellSize,
-                            y*cellSize,
-                            x*cellSize,
-                            (y+1)*cellSize,
-                            wallPaint);
-                if (cells[x][y].bottomWall)
-                    canvas.drawLine(
-                            x*cellSize,
-                            (y+1)*cellSize,
-                            (x+1)*cellSize,
-                            (y+1)*cellSize,
-                            wallPaint);
-                if (cells[x][y].rightWall)
-                    canvas.drawLine(
-                            (x+1)*cellSize,
-                            y*cellSize,
-                            (x+1)*cellSize,
-                            (y+1)*cellSize,
-                            wallPaint);
+
+            int width = getWidth();
+            int height = getHeight();
+
+            if (width / height < COLS / ROWS)
+                cellSize = width / (COLS + 1);
+            else
+                cellSize = height / (ROWS + 1);
+
+            hMargin = (width - COLS * cellSize) / 2;
+            vMargin = (height - ROWS * cellSize) / 2;
+
+            canvas.translate(hMargin, vMargin);
+
+            for (int x = 0; x < COLS; x++) {
+                for (int y = 0; y < ROWS; y++) {
+                    if (cells[x][y].topWall)
+                        canvas.drawLine(
+                                x * cellSize,
+                                y * cellSize,
+                                (x + 1) * cellSize,
+                                y * cellSize,
+                                wallPaint);
+                    if (cells[x][y].leftWall)
+                        canvas.drawLine(
+                                x * cellSize,
+                                y * cellSize,
+                                x * cellSize,
+                                (y + 1) * cellSize,
+                                wallPaint);
+                    if (cells[x][y].bottomWall)
+                        canvas.drawLine(
+                                x * cellSize,
+                                (y + 1) * cellSize,
+                                (x + 1) * cellSize,
+                                (y + 1) * cellSize,
+                                wallPaint);
+                    if (cells[x][y].rightWall)
+                        canvas.drawLine(
+                                (x + 1) * cellSize,
+                                y * cellSize,
+                                (x + 1) * cellSize,
+                                (y + 1) * cellSize,
+                                wallPaint);
+                }
             }
+
+            float margin = cellSize / 10;
+
+            canvas.drawRect(
+                    player.col * cellSize + margin,
+                    player.row * cellSize + margin,
+                    (player.col + 1) * cellSize - margin,
+                    (player.row + 1) * cellSize - margin,
+                    playerPaint);
+
+            canvas.drawRect(
+                    exit.col * cellSize + margin,
+                    exit.row * cellSize + margin,
+                    (exit.col + 1) * cellSize - margin,
+                    (exit.row + 1) * cellSize - margin,
+                    exitPaint);
         }
+        else{
+            size=80;
+            licznik = 1;
 
-        float margin = cellSize/10;
+            point.setTextSize(size);
 
-        canvas.drawRect(
-                player.col*cellSize+margin,
-                player.row*cellSize+margin,
-                (player.col+1)*cellSize-margin,
-                (player.row+1)*cellSize-margin,
-                playerPaint);
+            canvas.drawText("Wynik: " +  String.format("%.2f", punkty), 350, 900, point);
 
-        canvas.drawRect(
-                exit.col*cellSize+margin,
-                exit.row*cellSize+margin,
-                (exit.col+1)*cellSize-margin,
-                (exit.row+1)*cellSize-margin,
-                exitPaint);
+        }
     }
 
     private void movePlayer(Direction direction){
@@ -213,18 +255,22 @@ public class GameView extends View {
             case UP:
                 if(!player.topWall)
                     player = cells[player.col][player.row-1];
+                punkty=punkty+0.01;
                 break;
             case DOWN:
                 if(!player.bottomWall)
                     player = cells[player.col][player.row+1];
+                punkty=punkty+0.01;
                 break;
             case LEFT:
                 if(!player.leftWall)
                     player = cells[player.col-1][player.row];
+                punkty=punkty+0.01;
                 break;
             case RIGHT:
                 if(!player.rightWall)
                     player = cells[player.col+1][player.row];
+                punkty=punkty+0.01;
                 break;
 
         }
@@ -235,63 +281,62 @@ public class GameView extends View {
 
     private void checkExit(){
         if(player == exit) {
-          //  if(licznik_col%2==0 && licznik_col <= 8) {
-                COLS++;
-          //  }
             ROWS=ROWS+2;
-            licznik_col++;
-            punkty++;
+            COLS++;
+            punkty=punkty+5;
+            b=b-25;
             createMaze();
         }
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        if(event.getAction() == MotionEvent.ACTION_DOWN)
-            return true;
+        if (licznik == 0) {
+            if (event.getAction() == MotionEvent.ACTION_DOWN)
+                return true;
 
-        if(event.getAction() == MotionEvent.ACTION_MOVE){
-            float x = event.getX();
-            float y = event.getY();
+            if (event.getAction() == MotionEvent.ACTION_MOVE) {
+                float x = event.getX();
+                float y = event.getY();
 
-            float playerCenterX = hMargin + (player.col+0.5f)*cellSize;
-            float playerCenterY = hMargin + (player.row+0.5f)*cellSize;
+                float playerCenterX = hMargin + (player.col + 0.5f) * cellSize;
+                float playerCenterY = hMargin + (player.row + 0.5f) * cellSize;
 
-            float dx = x - playerCenterX;
-            float dy = y - playerCenterY;
+                float dx = x - playerCenterX;
+                float dy = y - playerCenterY;
 
-            float absDx = Math.abs(dx);
-            float absDy = Math.abs(dy);
+                float absDx = Math.abs(dx);
+                float absDy = Math.abs(dy);
 
-            if(absDx > cellSize || absDy > cellSize){
-                if(absDx > absDy){
-                    //move in x-direction
-                    if(dx > 0)
-                        movePlayer(Direction.RIGHT);
-                    else
-                        movePlayer(Direction.LEFT);
+                if (absDx > cellSize || absDy > cellSize) {
+                    if (absDx > absDy) {
+                        //move in x-direction
+                        if (dx > 0)
+                            movePlayer(Direction.RIGHT);
+                        else
+                            movePlayer(Direction.LEFT);
+                    } else {
+                        //move in y-direction
+                        if (dy > 0)
+                            movePlayer(Direction.DOWN);
+                        else
+                            movePlayer(Direction.UP);
+                    }
                 }
-                else{
-                    //move in y-direction
-                    if(dy > 0)
-                        movePlayer(Direction.DOWN);
-                    else
-                        movePlayer(Direction.UP);
-                }
+                return true;
             }
-            return true;
         }
-
         return super.onTouchEvent(event);
+
     }
 
     private class Cell{
         boolean
-            topWall = true,
-            leftWall = true,
-            bottomWall = true,
-            rightWall = true,
-            visited = false;
+                topWall = true,
+                leftWall = true,
+                bottomWall = true,
+                rightWall = true,
+                visited = false;
 
         int col, row;
 
